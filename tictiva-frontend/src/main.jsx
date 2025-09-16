88783cee3b9325335c4b7ebd233795a30c358247
Verónica Mateo <administracion@tictiva.com>
2025-09-15 22:33:20 -0300

    se modifica main

--- tictiva-frontend/src/main.jsx
+++ tictiva-frontend/src/main.jsx
@@ -20,8 +20,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </EmpresaProvider>
   </React.StrictMode>
-);import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
-window.supabase = supabase; // <- importante para DocumentosTab
+);
 
