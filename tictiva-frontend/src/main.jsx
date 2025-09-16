433ca10fd0bb2184851c92e29876b8cfbd4b1518
Verónica Mateo <administracion@tictiva.com>
2025-09-15 21:43:58 -0300

    se agrego supabase

--- tictiva-frontend/src/main.jsx
+++ tictiva-frontend/src/main.jsx
@@ -9,4 +9,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </EmpresaProvider>
   </React.StrictMode>
-);
+);import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+window.supabase = supabase; // <- importante para DocumentosTab
+
