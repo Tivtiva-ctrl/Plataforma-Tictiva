df82ff00dbe6b44be27915c2c7f71e9b1ba29c63
Verónica Mateo <administracion@tictiva.com>
2025-09-30 12:13:08 -0300

    modificacion en vercel

--- tictiva-frontend/vite.config.js
+++ tictiva-frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { "@": path.resolve(process.cwd(), "src") },
-    dedupe: ["react", "react-dom"],
+  base: "/",              // ⬅️ MUY IMPORTANTE (no "./")
+  build: {
+    outDir: "dist",       // o el que ya uses
   },
 });
