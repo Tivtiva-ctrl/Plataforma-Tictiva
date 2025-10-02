4bf000a1b749bbe58ed8092b63b4d6304bfbe46d
Verónica Mateo <administracion@tictiva.com>
2025-10-02 16:27:04 -0300

    prueba mil para fichas

--- tictiva-frontend/vite.config.js
+++ tictiva-frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",              // ⬅️ MUY IMPORTANTE (no "./")
-  build: {
-    outDir: "dist",       // o el que ya uses
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
-});
+})
